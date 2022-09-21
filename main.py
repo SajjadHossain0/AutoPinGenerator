2a6732e236ff4dc919e72690fa27eea5084b2adf
SajjadHossain0 <102672264+SajjadHossain0@users.noreply.github.com>
2022-09-21 14:38:29 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+import random
+'''
+lower = "abcdefghijklmnopqrstuvwxyz"
+upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+number = "1234567890"
+symbol = "!@#$%^&*()_+-=[]{};:'\|,.<>/?"
+all = lower+upper+number+symbol
+length = 16
+password = "".join(random.sample(all,length))
+print(password)
+'''
+number = "1234567890"
+length = 8
+password = "".join(random.sample(number,length))
+print(password)
